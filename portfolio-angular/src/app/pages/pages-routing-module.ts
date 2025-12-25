655147e6f86a3d0ec10fb91519914cc0b64eab16
Hithesh <hithesh.nandeesh@deliverycentric.com>
2025-12-25 21:44:47 +0530

    added new fonts, favicon icon, removed side nav layout

--- portfolio-angular/src/app/pages/pages-routing-module.ts
+++ portfolio-angular/src/app/pages/pages-routing-module.ts
@@ -1,25 +1,10 @@
 import { Routes } from '@angular/router';
 import { Entry } from './entry/entry';
 import { ReactShell } from './react-shell/react-shell';
-import { SideNavLayout } from '../layout/side-nav-layout/side-nav-layout';
 import { Context } from './context/context';
-import { About } from './about/about';
-import { Contact } from './contact/contact';
-import { Projects } from './projects/projects';
-import { Experience } from './experience/experience';
 
 export const PAGES_ROUTES: Routes = [
   { path: '', component: Entry },
   { path: 'react', component: ReactShell },
-  {
-    path: '',
-    component: SideNavLayout,
-    children: [
-      { path: 'context', component: Context },
-      { path: 'about', component: About },
-      { path: 'contact', component: Contact },
-      { path: 'projects', component: Projects },
-      { path: 'experience', component: Experience}
-    ],
-  },
+  { path: 'context', component: Context },
 ];
